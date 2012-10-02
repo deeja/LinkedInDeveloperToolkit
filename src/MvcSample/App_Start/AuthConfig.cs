48cd84c077bd1a782a1cd5aaa54e833bec95f35f
Daniel Blackwell <dannjb@gmail.com>
2012-10-02 08:02:39 +0000

    Upgrade Packages Fix OAuth for LinedInService

--- src/MvcSample/App_Start/AuthConfig.cs
+++ src/MvcSample/App_Start/AuthConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using LinkedIn.Mvc;
 using Microsoft.Web.WebPages.OAuth;
 using MvcSample.Models;
 
@@ -14,8 +15,11 @@ namespace MvcSample
             // To let users of this site log in using their accounts from other sites such as Microsoft, Facebook, and Twitter,
             // you must update this site. For more information visit http://go.microsoft.com/fwlink/?LinkID=252166
 
-         
-           // OAuthWebSecurity.RegisterLinkedInClient();
+
+            OAuthWebSecurity.RegisterClient(new LinkedInService("hs202okb7yph", "ZOenHuu0saj1ZWY9"),"Linked In",null);
+
+            OAuthWebSecurity.RegisterLinkedInClient("hs202okb7yph", "ZOenHuu0saj1ZWY9","Linked in OEM");
+            OAuthWebSecurity.RegisterGoogleClient("Google");
         }
     }
 }
